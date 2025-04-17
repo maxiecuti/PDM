4813392a24103fc16bc9f2a14a3012b8b37a332b
maxie <159179953+maxiecuti@users.noreply.github.com>
2025-04-17 13:27:17 -0300

    Update menu.dart

--- pooEmDart/menu.dart
+++ pooEmDart/menu.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+import 'barrel.dart';
+import 'ex1.dart';
+import 'ex2.dart';
+import 'ex3.dart';
+import 'ex4.dart';
+
 void exibirMenu(){
 
   print('''
@@ -11,4 +18,24 @@ void exibirMenu(){
   5. Sair do Programa
 
   ''');
+
+  void main(){
+    int escolha;
+    do{
+      exibirMenu();
+      stdout.write("Escolha:");
+      escolha = int.parse(stdin.readLineSync()!);
+
+      switch(escolha){
+        case 1: produto();
+          break;
+        case 2: contaBancaria();
+          break;
+        case 3: funcionario();
+          break;
+        case 4: aluno();
+          break;
+      }
+    }while (escolha != 9);
+  }
 }
